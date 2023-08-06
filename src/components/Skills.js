56c42ec35f33af4af118fba17f003290227bf503
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-08-05 22:09:32 -0500

    Skills.js updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,7 @@
 // src/components/Skills.js
 
-import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
 import { skills } from "../data";
 
 export default function Skills() {
@@ -9,7 +9,10 @@ export default function Skills() {
     <section id="skills">
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
+          <FontAwesomeIcon
+            icon={["fab", "fort-awesome"]} // Example Font Awesome icon
+            className="w-10 inline-block mb-4"
+          />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Skills &amp; Technologies
           </h1>
@@ -21,11 +24,14 @@ export default function Skills() {
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
-            <div key={skill} className="p-2 sm:w-1/2 w-full">
+            <div key={skill.name} className="p-2 sm:w-1/2 w-full">
               <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <FontAwesomeIcon
+                  icon={["fab", skill.icon]} // Use the icon class from skills
+                  className="text-green-400 w-6 h-6 flex-shrink-0 mr-4"
+                />
                 <span className="title-font font-medium text-white">
-                  {skill}
+                  {skill.name}
                 </span>
               </div>
             </div>
