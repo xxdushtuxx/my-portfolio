35f029f1608e676532f259bf3da537ccfb7eb577
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-08-05 21:36:59 -0500

    Bug fixed in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-/ src/components/Navbar.js
+// src/components/Navbar.js
 
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
@@ -25,7 +25,8 @@ export default function Navbar() {
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
